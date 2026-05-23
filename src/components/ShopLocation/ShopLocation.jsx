cba297831c352ea63a82e1220e9a6af777bff8bc
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2026-05-23 14:41:08 +0530

    add location

--- src/components/ShopLocation/ShopLocation.jsx
+++ src/components/ShopLocation/ShopLocation.jsx
@@ -31,7 +31,7 @@ export default function ShopLocationSection() {
             {/* Get Directions Button */}
             <div className="mt-6">
               <a
-                href="https://maps.app.goo.gl/vFmev1hEuVd79Tno9"
+                href="https://maps.app.goo.gl/z7q2Go2RFD2mCuJu7"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-block bg-indigo-600 hover:bg-indigo-700 text-white text-sm md:text-base font-semibold py-2 px-4 rounded-xl shadow-md transition"
