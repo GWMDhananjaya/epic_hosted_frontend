cd373d5251ccc99bc3c1d60a509a196aa55d8ded
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2026-05-27 00:27:33 +0530

    update url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(), flowbiteReact()],
-  base: "/epic_hosted_frontend/", // Add this line for GitHub Pages
+  base: "/",
   build: {
     outDir: "dist",
     sourcemap: false,
